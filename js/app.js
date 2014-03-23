8bad1375fedca2fffa077a882814fbf81a5f23c6
Mark Miro <contact@markmiro.com>
2014-03-23 13:51:40 -0700

    Created implementation of a toggle block. Only tested in Chrome
    Also got rid of the about section next to the skills and moved it up.
    Made the additional skills smaller

--- js/app.js
+++ js/app.js
@@ -5,4 +5,36 @@ $(function () {
     //     body: "Lorem ipsum Eu dolor ea Duis id occaecat eu pariatur aliquip sit cillum laborum minim deserunt magna ex mollit commodo velit culpa."
     // }));
     // $('#content').html("Hello!");
+    count = 0;
+
+    // var oldPosition = 0;
+    $('.toggle-link').click(function(e) {
+        var $target = $(e.target);
+        var $block = $('.toggle-block');
+        $block.toggleClass('active');
+        if ($block.hasClass('active')) {
+            var position = $(window).scrollTop();
+            $(e.target).text('less...');
+            $target.data('old-position', position);
+            var scrollHandler = function () {
+                count++;
+                var height = $block.outerHeight();
+                if ($(window).scrollTop() > $block.position().top + height) {
+                    $target.text('more...');
+                    $block.removeClass('active');
+                    $target.removeData('old-position');
+                    $.scrollTo('-='+height);
+                    $(window).off('scroll', scrollHandler);
+                    // $block.css('background', 'red');
+                }
+            }
+            $(window).scroll(scrollHandler);
+            // $.scrollTo(position + 10, 400);
+        } else {
+            $target.text('more...');
+            var oldPosition = parseInt($target.data('old-position'));
+            $target.removeData('old-position');
+            $.scrollTo(oldPosition + 10).scrollTo(oldPosition, 400);
+        }
+    });
 });
