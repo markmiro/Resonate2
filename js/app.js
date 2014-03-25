4fb2ac4240af8903555296a9cb83a0ea887bc478
Mark Miro <contact@markmiro.com>
2014-03-24 18:44:00 -0700

    Made toggle able sections for portfolio. Now have flat iOS devices

--- js/app.js
+++ js/app.js
@@ -5,9 +5,40 @@ $(function () {
     //     body: "Lorem ipsum Eu dolor ea Duis id occaecat eu pariatur aliquip sit cillum laborum minim deserunt magna ex mollit commodo velit culpa."
     // }));
     // $('#content').html("Hello!");
-    count = 0;
 
-    // var oldPosition = 0;
+    var $project = $('.project');
+    // var collapsable = [
+    // '.name',
+    // '.description',
+    // '.logo',
+    // 'p',
+    // 'h2',
+    // 'h3',
+    // 'h4',
+    // 'h5',
+    // 'h6'
+    // ];
+    // // for (var i = 0; i < collapsable.length; i++) {
+    // //     var partialSelector = collapsable[i];
+    // //     var $item = $project.find(partialSelector);
+    // //     // $item.data('height', $item.outerHeight());
+    // //     // var $item = $('project.collapsed').find(partialSelector);
+    // //     // $item.css('max-height', $item.outerHeight());
+    // // };
+    $project.addClass('collapsed');
+    $previousTarget = null;
+
+    $('.project.collapsed').click(function (e) {
+        var $target = $(this);
+        $target.removeClass('collapsed');
+        if ($previousTarget != null && $previousTarget != $target) {
+            var height = $previousTarget.outerHeight();
+            $previousTarget.addClass('collapsed');
+        }
+        $.scrollTo($target);
+        $previousTarget = $target;
+    });
+
     $('.toggle-link').click(function(e) {
         var $target = $(e.target);
         var $block = $('.toggle-block');
@@ -17,7 +48,6 @@ $(function () {
             $(e.target).text('less...');
             $target.data('old-position', position);
             var scrollHandler = function () {
-                count++;
                 var height = $block.outerHeight();
                 if ($(window).scrollTop() > $block.position().top + height) {
                     $target.text('more...');
@@ -37,4 +67,5 @@ $(function () {
             $.scrollTo(oldPosition + 10).scrollTo(oldPosition, 400);
         }
     });
+
 });
