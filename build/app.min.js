3ac02dd5eb13c427bb6a35fdb38d153782c5c47f
Mark Miro <contact@markmiro.com>
2014-05-01 08:39:06 -0700

    added ajax

--- build/app.min.js
+++ build/app.min.js
@@ -1,19 +1,53 @@
 $(function () {
 
+    // $(".email-form").submit(function(e) {
+    //     e.preventDefault();
+    //     alert($(".email-form").serialize());
+    //     $.ajax({
+    //             type: "GET",
+    //             url: "send-mail.php",
+    //             data: $(".email-form").serialize(),
+    //             success:function(response) { alert("Thanks for signing up!"); },
+    //             error: function () { alert("There was an error signing you up. Please make sure all your information is correct.") }
+    //        });
+       
+    // });
+
+
     $(".email-form").submit(function(e) {
         e.preventDefault();
-        alert($(".email-form").serialize());
-        $.ajax({
-                type: "GET",
-                url: "send-mail.php",
-                data: $(".email-form").serialize(),
-                success:function(response) {
-                        alert("Thanks for signing up!");
-                    },
-                error: function () { alert("There was an error signing you up. Please make sure all your information is correct.") }
-           });
-       
-    });
+        
+        var firstName = $(".name-field").val();
+        var email = $(".email-field").val();
+        
+        
+        // alert('Submitting ' + firstName + ' email: ' + email);
+        
+        var xmlhttp;
+        if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+          }
+        else
+          {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+        xmlhttp.onreadystatechange=function()
+          {
+          if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                 alert('Thanks for signing up');
+            }
+          else {
+            alert('Oops! Something went wrong. Try again...');
+          }
+          }
+        xmlhttp.open("GET","http://www.surplusbidder.com/send-mail.php?e=" + email + "&f=" + firstName ,true);
+        xmlhttp.send();
+
+               
+         });
+
 
 
 });
